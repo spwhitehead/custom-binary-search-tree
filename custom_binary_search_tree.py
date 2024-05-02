d9e4357767c5f19723fd862b25c34b7c82d5f06a
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-05-02 15:03:36 -0600

    Implements through find min and max

--- custom_binary_search_tree.py
+++ custom_binary_search_tree.py
@@ -1,37 +1,133 @@
 # Implement a BST. It should include these methods:
+# class Node():
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+class Node():
+    def __init__(self, value):
+        self.value: int = value
+        self.left: Node = None
+        self.right: Node = None
+
 class BST():
-    def __init__():
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def insert(self, int) -> None:
+    def insert(self, value: int):
         """inserts a number into the tree"""
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.ins_recursive(value, self.root)
+      
 
-        pass
+    def ins_recursive(self, value: int, current_node: Node):
+        if value < current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self.ins_recursive(value, current_node.left)
+        else:
+            if value > current_node.value:
+                if current_node.right is None:
+                    current_node.right = Node(value)
+                else:
+                    self.ins_recursive(value, current_node.right)
+            
 
-    def search(self, int) -> bool:
+    def search(self, value: int) -> bool:
         """returns whether or not a value is in the tree"""
-        pass
+        if self.root is None:
+            return False
+        else:
+            return self.search_recursive(value, self.root)
 
-    def in_order_traversal(self) -> list[int]:
+    def search_recursive(self, value: int, current_node: Node):
+        if current_node.value == value:
+            return True
+        else:
+            if value < current_node.value:
+                if current_node.left == None:
+                    return False
+                else:
+                    return self.search_recursive(value, current_node.left)
+                         
+            if value > current_node.value:
+                if current_node.right == None:
+                    return False
+                else: 
+                    return self.search_recursive(value, current_node.right)
+
+
+
+    def in_order_traversal(self, current_node: Node=None) -> list[int]:
         """returns a list of all the values in sorted order"""
-        pass
+        values = []
+        v_left = []
+        v_right = []
+        if current_node == None:
+            current_node = self.root
+        if current_node == None:
+            return []
 
-    def find_min(self) -> int:
+        else:
+            if current_node.left != None:
+                v_left = self.in_order_traversal(current_node.left)   
+
+            v_left.append(current_node.value)
+
+            if current_node.right != None:
+                v_right = self.in_order_traversal(current_node.right)
+            
+            values = v_left + v_right
+            
+            return values
+
+
+    def find_min(self, current_node: Node=None) -> int:
         """returns the smallest number in the tree(you cannot turn the tree into a list then return an element from the list, you must do it by traversing the tree)"""
-        pass
+        if current_node == None:
+            current_node = self.root
 
-    def find_max(self) -> int:
+        if self.root == None:
+            return current_node.value
+        
+        while current_node.left != None:
+            current_node = current_node.left
+        return current_node.value
+
+
+    def find_max(self, current_node: Node=None) -> int:
         """returns the largest number in the tree(you cannot turn the tree into a list then return an element from the list, you must do it by traversing the tree)"""
-        pass
+        if current_node == None:
+            current_node = self.root
+
+        if self.root == None:
+            return current_node.value
+        
+        while current_node.right != None:
+            current_node = current_node.right
+        return current_node.value 
+
 
     def height(self) -> int:
         """returns the depth of the tree(how far is the furthest node from the root node?)"""
         pass
 
-    def count_leaves(self) -> int:
+    def count_leaves(self, current_node: Node=None) -> int:
         """returns the number of leaf nodes in the tree(leaf nodes are nodes without any children)"""
-        pass
-
+        if current_node == None:
+            current_node = self.root
+        if current_node == None:
+            return 0
+        
+        # ### copy in-order traversal code ###
+        leaf_count = 0
+        if current_node.left == None and current_node.right == None:
+            count += 1
+        
+        
     def serialize(self) -> str:
         """turn the BST into a string"""
         pass
--- test_custom_binary_search_tree.py
+++ test_custom_binary_search_tree.py
@@ -19,30 +19,35 @@ def test_insert():
     assert tree.search(4) == False
     tree.insert(4)
     assert tree.search(4)
+    assert tree.root is not None, "root not none"
+    assert tree.root.value == 4, "root should = 4"
 
 
 def test_search(tree: BST):
     """returns whether or not a value is in the tree"""
+    assert tree.root is not None, "root should not be none after insertion"
+    assert tree.root.value == 2, "root value should be the first inserted node"
     assert tree.search(4) == False
-    assert tree.search(2)
-    assert tree.search(3)
-    assert tree.search(9)
-    assert tree.search(6)
+    assert tree.search(2) == True
+    assert tree.search(3) == True
+    assert tree.search(9) == True
+    assert tree.search(6) == True
+    assert tree.search(0) == False
 
 
 def test_in_order_traversal(tree: BST):
     """returns a list of all the values in sorted order"""
-    expected = [2, 3, 4, 6, 9]
+    expected = [2, 3, 6, 9]
     actual = tree.in_order_traversal()
     assert expected == actual
-
-
+ 
+ 
 def test_find_min(tree: BST):
     """returns the smallest number in the tree(you cannot turn the tree into a list then return an element from the list, you must do it by traversing the tree)"""
     expected = 2
     actual = tree.find_min()
     assert expected == actual
-
+ 
 
 def test_find_max(tree: BST):
     """returns the largest number in the tree(you cannot turn the tree into a list then return an element from the list, you must do it by traversing the tree)"""
